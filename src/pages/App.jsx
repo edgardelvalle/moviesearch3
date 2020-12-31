098410ed9b7efb033490b5e645a33a8c73e34fc7
Edgar Del Valle <edqardv@gmail.com>
2020-12-31 12:56:26 -0800

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -41,13 +41,13 @@ const App = () => {
         <Route exact path="/">
           <Redirect to="/discover/popular" />
         </Route>
-        <Route exact path="/moviesearch3/search/:searchedMovies">
+        <Route exact path="/search/:searchedMovies">
           <Search />
         </Route>
-        <Route exact path="/moviesearch3/movie/:id">
+        <Route exact path="/movie/:id">
           <Movie />
         </Route>
-        <Route exact path="/moviesearch3/discover/:discover">
+        <Route exact path="/discover/:discover">
           <Discovery />
         </Route>
       </Router>
